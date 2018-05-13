a88686e6889a2b54b00929ba05ccd8a50c1b3622
Mateusz Bąkała <maba@netcompany.com>
2018-05-13 09:46:22 +0200

    Generating demands with beta distribution

---
+++ VrpTestCasesGenerator.Tests/DemandGeneratorTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VrpTestCasesGenerator.Generator;
+
+namespace VrpTestCasesGenerator.Tests
+{
+    [TestClass]
+    public class DemandGeneratorTests
+    {
+        [TestMethod]
+        public void DemeandGenerationTest()
+        {
+            var gen = new DemandGenerator(0.1, 1, 1000);
+            var demands = gen.GenerateDemands(10);
+            foreach (var demand in demands)
+            {
+                Assert.IsTrue(demand >0 && demand < 1000);
+            }
+        }
+    }
+}
---
+++ VrpTestCasesGenerator/Generator/DemandGenerator.cs
@@ -0,0 +1,31 @@
+﻿using Accord.Statistics.Distributions.Univariate;
+
+namespace VrpTestCasesGenerator.Generator
+{
+    public interface IDemandGenerator
+    {
+        int[] GenerateDemands(int clients);
+    }
+
+    public class DemandGenerator : IDemandGenerator
+    {
+        private readonly BetaDistribution _betaDistribution;
+        private readonly int _capacity;
+
+        public DemandGenerator(double alpha, double beta, int capacity)
+        {
+            _betaDistribution = new BetaDistribution(alpha, beta);
+            _capacity = capacity;
+        }
+
+        public int[] GenerateDemands(int clients)
+        {
+            var demands = new int[clients];
+            for (int i = 0; i < clients; i++)
+            {
+                demands[i] = (int)(1+_betaDistribution.Generate() * (_capacity-1)); //to have result from 1 to capacity
+            }
+            return demands;
+        }
+    }
+}
